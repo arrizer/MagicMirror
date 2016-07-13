7122cfd2744de3576e2d1df1d23ee1ad493ae54a
Matthias Schwab <matthias@matthiasschwab.de>
2016-07-13 20:28:32 +0200

    Startup script now silent

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 killall chromium
 killall node
 
-coffee /home/pi/Projects/MagicMirror/app.coffee &
-sleep 15 && chromium --kiosk "http://localhost:8080" &
+coffee /home/pi/Projects/MagicMirror/app.coffee 2>&1 > /dev/null &
+sleep 15 && chromium --kiosk "http://localhost:8080" 2>&1 > /dev/null &
