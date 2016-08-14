cfcc5a935e42448ba7b414fd4d2125d0eaf7b8a8
Matthias Schwab <matthias@matthiasschwab.de>
2016-08-14 16:58:10 +0200

    Disbling chromium infobars on startup

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ killall node
 cd /home/pi/Projects/MagicMirror/
 pm2 stop app.coffee
 pm2 start app.coffee
-sleep 15 && chromium --kiosk "http://localhost:8080" 2>&1 > /dev/null &
+sleep 15 && chromium --disable-session-crashed-bubble --disable-infobars --kiosk "http://localhost:8080" 2>&1 > /dev/null &
