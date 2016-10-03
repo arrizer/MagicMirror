62e2e43936e6e2624268037ae52e63f3d18b7421
Matthias Schwab <matthias@matthiasschwab.de>
2016-10-03 18:32:29 +0200

    Starting chromium twice to get rid of annoying yellow bars

--- startup.sh
+++ startup.sh
@@ -6,4 +6,8 @@ killall node
 cd /home/pi/Projects/MagicMirror/
 pm2 stop app.coffee
 pm2 start app.coffee
-sleep 15 && chromium --disable-session-crashed-bubble --disable-infobars --kiosk "http://localhost:8080" 2>&1 > /dev/null &
+chromium &
+sleep 5
+killall chromium
+sleep 10
+chromium --disable-session-crashed-bubble --disable-infobars --kiosk "http://localhost:8080" 2>&1 > /dev/null &
