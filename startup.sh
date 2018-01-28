f1ad1b63168221264fbf2fdba8e4573d8e536b4b
Matthias Schwab <matthias@matthiasschwab.de>
2018-01-28 15:42:06 +0100

    Use sudo

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 killall chromium-browser
 cd /home/pi/MagicMirror/
-pm2 stop app.coffee
-pm2 start app.coffee
+sudo pm2 kill
+sudo pm2 start app.coffee
 sleep 5
 sh /home/pi/scripts/start_chromium_browser
